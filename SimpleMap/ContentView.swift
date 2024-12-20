2795d21a11874bbc5ae2862812f97d91a0be4505
Yehor Myropoltsev <egormiropoltsev79@gmail.com>
2024-12-20 20:31:29 +0200

    Initial Commit

---
+++ SimpleMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleMap
+//
+//  Created by Yehor Myropoltsev on 20.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleMap/SimpleMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMapApp.swift
+//  SimpleMap
+//
+//  Created by Yehor Myropoltsev on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
